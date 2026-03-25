e2cb4952f4abb28c3ed708114d51ec8fed7569ee
Kipraslesk <23qf31@queensu.ca>
2026-03-25 19:04:31 -0400

    Changed functions to require instead of create

--- main.py
+++ main.py
@@ -9,36 +9,19 @@ print ("fuuhhh")
 with h5py.File('data') as hdf:
 
     #Create the main three data groups
-    if 'Raw_Data' not in hdf:
-        raw_group = hdf.create_group('Raw_Data')
-    else:
-        raw_group = hdf['Raw_Data']
+    raw_group = hdf.require_group('Raw_Data')
 
-    if 'Processed_Data' not in hdf:
-        processed_group = hdf.create_group('Processed_Data')
-    else:
-        processed_group = hdf['Processed_Data']
+    processed_group = hdf.require_group('Processed_Data')
 
-    if 'Segmented_Data' not in hdf:
-        segmented_group = hdf.create_group('Segmented_Data')
-    else:
-        segmented_group = hdf['Segmented_Data']
+    segmented_group = hdf.require_group('Segmented_Data')
 
     #Fill group member data into raw_group
-    if 'Kipras' not in raw_group:
-        member_group = raw_group.create_group('Kipras')
-    else:
-        member_group = raw_group['Kipras']
+    member_group = raw_group.require_group('Kipras')
 
-    if 'Umair' not in raw_group:
-        member_group = raw_group.create_group('Umair')
-    else:
-        member_group = raw_group['Umair']
+    member_group = raw_group.require_group('Umair')
 
-    if 'Larry' not in raw_group:
-        member_group = raw_group.create_group('Larry')
-    else:
-        member_group = raw_group['Larry']
+    member_group = raw_group.require_group('Larry')
+
 
 
 
