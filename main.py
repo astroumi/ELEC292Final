9930a348f9dcdc4b62441f407a310e798fab6e66
Kipraslesk <23qf31@queensu.ca>
2026-03-25 18:58:15 -0400

    Created the 3 main subgroups

--- main.py
+++ main.py
@@ -6,10 +6,40 @@ import numpy as np
 
 print ("fuuhhh")
 
-with h5py.File('C:\Users\23qf31\ELEC 292\292_Final_Project_Files\RawAccelerationData') as hdf:
+with h5py.File('data') as hdf:
 
-    raw_group = hdf.create_group('Raw_Data')
-    processed_group = hdf.create_group('Processed_Data')
-    segmented_group = hdf.create_group('Segmented_Data')
+    #Create the main three data groups
+    if 'Raw_Data' not in hdf:
+        raw_group = hdf.create_group('Raw_Data')
+    else:
+        raw_group = hdf['Raw_Data']
+
+    if 'Processed_Data' not in hdf:
+        processed_group = hdf.create_group('Processed_Data')
+    else:
+        processed_group = hdf['Processed_Data']
+
+    if 'Segmented_Data' not in hdf:
+        segmented_group = hdf.create_group('Segmented_Data')
+    else:
+        segmented_group = hdf['Segmented_Data']
+
+    #Fill group member data into raw_group
+    if 'Kipras' not in raw_group:
+        member_group = raw_group.create_group('Kipras')
+    else:
+        member_group = raw_group['Kipras']
+
+    if 'Umair' not in raw_group:
+        member_group = raw_group.create_group('Umair')
+    else:
+        member_group = raw_group['Umair']
+
+    if 'Larry' not in raw_group:
+        member_group = raw_group.create_group('Larry')
+    else:
+        member_group = raw_group['Larry']
+
+
 
 #love kip<3
