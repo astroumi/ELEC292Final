b28d39ee5ac3f1bcfbb4a4fb0c0e10a4cd7bc044
Kipraslesk <23qf31@queensu.ca>
2026-03-29 14:44:11 -0400

    Worked on Preprocesssing and Visualization

--- main.py
+++ main.py
@@ -2,6 +2,72 @@ import h5py
 import pandas as pd
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+
+def preprocessing(raw_data):
+
+    #raw_data_array: [Time, X, Y, Z, Abs]
+    time = raw_data[:,0]
+
+    #Linearly imputate missing data
+    df_axes = pd.DataFrame(raw_data[:, 1:4], columns=['X', 'Y', 'Z'])
+    df_axes = df_axes.interpolate(method='linear', limit_direction='both')
+
+    #Remove offset from gravity
+    #Subtracts the mean from each axis to center the raw data around 0 m/s^2
+    no_gravity = df_axes.values - np.mean(df_axes.values, axis=0)
+
+    #Filter out high frequency noise with moving average
+    #A window of size 5 is used
+    df_smoothed = pd.DataFrame(no_gravity, columns=['X', 'Y', 'Z'])
+    df_smoothed = df_smoothed.rolling(window=5, center=True, min_periods=1).mean()
+
+    #Calculate magnitude of acceleration from smooth X, Y, Z
+    mag = np.sqrt(df_smoothed['X']**2 + df_smoothed['Y']**2 + df_smoothed['Z']**2)
+
+    #Build the processed dataframe
+    processed_df = pd.DataFrame({
+        'time': time,
+        'X': df_smoothed['X'],
+        'Y': df_smoothed['Y'],
+        'Z': df_smoothed['Z'],
+        'Magnitude': mag
+    })
+
+    return processed_df
+
+#Creates a figure with 4 sublots for X, Y, Z and magnitude stacked vertical
+def plot_accel_data(df, title="Accelerometer Data")
+    #Sets up 4-row grid
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(10, 12), sharex=True)
+
+    #Plot X-Axis
+    ax1.plot(df['time'], df['X'], color='red', label='X-axis')
+    ax1.set_ylabel('X (m/s²)')
+    ax1.grid(True, alpha=0.3)
+    ax1.set_title('X-axis acceleration vs time')
+
+    #Plot Y-axis
+    ax2.plot(df['time'], df['Y'], color='blue', label='Y-axis')
+    ax2.set_ylabel('Y (m/s²)')
+    ax2.grid(True, alpha=0.3)
+    ax2.set_title('Y-axis acceleration vs time')
+
+    #Plot Z-axis
+    ax3.plot(df['time'], df['Z'], color='green', label='Z-axis')
+    ax3.set_zlabel('Z')
+    ax3.grid(True, alpha=0.3)
+    ax3.set_title('Z-axis acceleration vs time')
+
+    #Plot Magnitude
+    ax4.plot(df['time'], df['Magnitude'], color='purple', label='Magnitude')
+    ax4.set_ylabel('Magnitude')
+    ax4.grid(True, alpha=0.3)
+    ax4.set_title('Magnitude of acceleration vs time')
+
+    plt.tight_layout()
+    plt.show()
+
 
 print ("fuuhhh")
 
@@ -65,12 +131,29 @@ with h5py.File(h5_path, 'a') as hdf:
             #Creates activity folder
             activity_group = member_group.require_group(activity)
 
-    #Segmented Data Group Set-up
-    for type in ['Train', 'Test']:
-        #Creates path to type folder
-        type_path = os.path.join(member_path, type)
-        #Creates type folder
-        type_path = member_group.require_group(type)
+            #Loop through every dataset stored in the raw group
+            for dataset_name in raw_group[member_name][activity]:
+                #Get the 2D array from the raw group
+                raw_data = raw_group[member_name][activity][dataset_name][:]
+                #Process the information inside
+                processed_signal = preprocessing(raw_data)
+
+                #Delete duplicates
+                if dataset_name in activity_group:
+                    del activity_group[dataset_name]
+                activity_group.create_dataset(dataset_name, data=processed_signal)
+
+print("Preprocessing complete.")
+
+
+
+
+    # #Segmented Data Group Set-up
+    # for type in ['Train', 'Test']:
+    #     #Creates path to type folder
+    #     type_path = os.path.join(member_path, type)
+    #     #Creates type folder
+    #     type_path = member_group.require_group(type)
 
 
 
